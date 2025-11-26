7a9e18cf43ee54dc6452d500fad33d60caca948b
Roshane Moraes <moraes.git@gmail.com>
2025-11-26 12:41:02 +0000

    update AsyncOperationStatusMgtRestClient

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/restclients/AsyncOperationStatusMgtRestClient.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/restclients/AsyncOperationStatusMgtRestClient.java
@@ -6,7 +6,7 @@
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,14 +18,16 @@
 
 package org.wso2.identity.integration.test.restclients;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.wso2.carbon.automation.engine.context.AutomationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.v1.model.Operation;
+import org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.v1.model.Operations;
+import org.wso2.carbon.identity.api.server.asynchronous.operation.status.management.v1.model.UnitOperations;
 import org.wso2.carbon.automation.engine.context.beans.Tenant;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 import org.wso2.identity.integration.test.utils.OAuth2Constant;
@@ -34,7 +36,7 @@ import java.io.Closeable;
 import java.io.IOException;
 
 /**
- * REST client for the Asynchronous Operation Status Management APIs.
+ * REST client for asynchronous operation status management APIs.
  */
 public class AsyncOperationStatusMgtRestClient extends RestBaseClient implements Closeable {
 
@@ -42,88 +44,76 @@ public class AsyncOperationStatusMgtRestClient extends RestBaseClient implements
     private static final String OPERATIONS_PATH = "operations";
     private static final String UNIT_OPERATIONS_PATH = "units";
 
-    private final Tenant tenantInfo;
-    private final OAuth2RestClient oAuth2RestClient;
-    private final String asyncStatusBasePath;
+    private final String serverUrl;
     private final String tenantDomain;
-    private final String baseUrl;
+    private final String basePath;
+    private final String username;
+    private final String password;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public AsyncOperationStatusMgtRestClient(String serverUrl, AutomationContext context) {
+    public AsyncOperationStatusMgtRestClient(String serverUrl, Tenant tenantInfo) {
 
-        this.tenantInfo = context.getContextTenant();
-        this.baseUrl = serverUrl;
-        this.tenantDomain = tenantInfo.getDomain();
-        this.oAuth2RestClient = new OAuth2RestClient(serverUrl, tenantInfo);
-        this.asyncStatusBasePath = getAsyncStatusBasePath();
+        this.serverUrl = serverUrl;
+        this.tenantDomain = tenantInfo.getContextUser().getUserDomain();
+        this.username = tenantInfo.getContextUser().getUserName();
+        this.password = tenantInfo.getContextUser().getPassword();
+        this.basePath = resolveBasePath();
     }
 
     /**
-     * Retrieve the list of operations for the tenant.
+     * Retrieve asynchronous operations created within the tenant.
      */
-    public JSONArray getOperations() throws Exception {
+    public Operations getOperations() throws Exception {
 
-        try (CloseableHttpResponse response = getResponseOfHttpGet(buildOperationsPath(), getHeadersWithBearerToken())) {
+        try (CloseableHttpResponse response = getResponseOfHttpGet(basePath + PATH_SEPARATOR + OPERATIONS_PATH,
+                getHeadersWithBasicAuth())) {
             String responseString = EntityUtils.toString(response.getEntity());
-            return getJSONArray(responseString);
+            return objectMapper.readValue(responseString, Operations.class);
         }
     }
 
     /**
-     * Retrieve a single operation by correlation ID.
+     * Retrieve a specific asynchronous operation by correlation identifier.
      */
-    public JSONObject getOperation(String correlationId) throws Exception {
+    public Operation getOperation(String correlationId) throws Exception {
 
-        try (CloseableHttpResponse response = getResponseOfHttpGet(buildOperationPath(correlationId),
-                getHeadersWithBearerToken())) {
+        String endpoint = basePath + PATH_SEPARATOR + OPERATIONS_PATH + PATH_SEPARATOR + correlationId;
+        try (CloseableHttpResponse response = getResponseOfHttpGet(endpoint, getHeadersWithBasicAuth())) {
             String responseString = EntityUtils.toString(response.getEntity());
-            return getJSONObject(responseString);
+            return objectMapper.readValue(responseString, Operation.class);
         }
     }
 
     /**
-     * Retrieve unit operation records for a given correlation ID.
+     * Retrieve unit operation records for a given correlation identifier.
      */
-    public JSONArray getUnitOperations(String correlationId) throws Exception {
+    public UnitOperations getUnitOperations(String correlationId) throws Exception {
 
-        try (CloseableHttpResponse response = getResponseOfHttpGet(buildOperationPath(correlationId) +
-                PATH_SEPARATOR + UNIT_OPERATIONS_PATH, getHeadersWithBearerToken())) {
+        String endpoint = basePath + PATH_SEPARATOR + OPERATIONS_PATH + PATH_SEPARATOR + correlationId +
+                PATH_SEPARATOR + UNIT_OPERATIONS_PATH;
+        try (CloseableHttpResponse response = getResponseOfHttpGet(endpoint, getHeadersWithBasicAuth())) {
             String responseString = EntityUtils.toString(response.getEntity());
-            return getJSONArray(responseString);
+            return objectMapper.readValue(responseString, UnitOperations.class);
         }
     }
 
-    private String buildOperationsPath() {
+    private String resolveBasePath() {
 
-        return asyncStatusBasePath + PATH_SEPARATOR + OPERATIONS_PATH;
-    }
-
-    private String buildOperationPath(String correlationId) {
-
-        return buildOperationsPath() + PATH_SEPARATOR + correlationId;
-    }
-
-    private String getAsyncStatusBasePath() {
-
-        String tenantDomain = tenantInfo.getDomain();
         if (StringUtils.equals(tenantDomain, MultitenantConstants.SUPER_TENANT_DOMAIN_NAME)) {
-            return baseUrl + PATH_SEPARATOR + API_SERVER_PATH + PATH_SEPARATOR + ASYNC_OPERATION_STATUS_PATH;
+            return serverUrl + API_SERVER_PATH + PATH_SEPARATOR + ASYNC_OPERATION_STATUS_PATH;
         }
-        return baseUrl + PATH_SEPARATOR + TENANT_PATH + tenantDomain + PATH_SEPARATOR + API_SERVER_PATH +
-                PATH_SEPARATOR + ASYNC_OPERATION_STATUS_PATH;
+        return serverUrl + TENANT_PATH + tenantDomain + PATH_SEPARATOR + API_SERVER_PATH + PATH_SEPARATOR +
+                ASYNC_OPERATION_STATUS_PATH;
     }
 
-    private Header[] getHeadersWithBearerToken() throws Exception {
+    private Header[] getHeadersWithBasicAuth() {
 
+        String credentials = Base64.encodeBase64String((username + ":" + password).getBytes()).trim();
         return new Header[] {
                 new BasicHeader(USER_AGENT_ATTRIBUTE, OAuth2Constant.USER_AGENT),
-                new BasicHeader(AUTHORIZATION_ATTRIBUTE, BEARER_TOKEN_AUTHORIZATION_ATTRIBUTE + getAccessToken()),
+                new BasicHeader(AUTHORIZATION_ATTRIBUTE, BASIC_AUTHORIZATION_ATTRIBUTE + credentials),
                 new BasicHeader(CONTENT_TYPE_ATTRIBUTE, "application/json")
         };
-    }
-
-    private String getAccessToken() throws Exception {
-
-        return oAuth2RestClient.getM2MAccessToken();
     }
 
     @Override
